3577f7692ad960c8c17ba2efbeefd1dcc6955869
aniram112 <masyakmarina@gmail.com>
2022-03-20 17:54:17 +0300

    Initial Commit

---
+++ maroschupkina_1PW8/Controllers/MoviesViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  maroschupkina_1PW8
+//
+//  Created by Marina Roshchupkina on 18.03.2022.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+    private let tableView = UITableView()
+    private let apiKey = "52846410a29857ff5ee4a418ab6ab056"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            self?.loadMovies()
+        }
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configureUI() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.register(MovieView.self, forCellReuseIdentifier: MovieView.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        tableView.reloadData()
+    }
+    
+    private func loadMovies() {
+        guard let url = URL(string: "https://api.themoviedb.org/3/discover/movie?api_key=\(apiKey)&language=ruRu") else{
+            return assertionFailure("some problems")
+        }
+        
+        let session = URLSession.shared.dataTask(with: URLRequest(url: url), completionHandler: {data, _, _ in
+            
+        })
+        session.resume()
+    }
+    
+    
+}
+
+extension MoviesViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return MovieView()
+    }
+    
+}
---
+++ maroschupkina_1PW8/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  maroschupkina_1PW8
+//
+//  Created by Marina Roshchupkina on 19.03.2022.
+//
+
+import UIKit
+
+struct Movie {
+    let title: String
+    let posterPath: String
+    let poster: UIImage?
+}
